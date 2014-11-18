5d02e043d08eab6721e101707c3155cc9da6af02
Gabriel Such <gsuch@salesforce.com>
2014-11-18 15:37:02 -0800

    add the playground safety surface.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clojure-playground "1.0.0-SNAPSHOT"
+:dependencies [[org.clojure/clojure "1.6.0"]
+	       [midje "1.6.3"]]
+:profiles {:dev {:plugins [[lein-midje "3.1.3"]]}})
---
+++ test/clojure_playground.clj
@@ -0,0 +1,5 @@
+(ns clojure-playground
+  (:use midje.sweet))
+
+(facts "My first fact"
+   (== 1 1) => true)
